55418814ec0f03ff3d424af4a63a6939cf9275db
Oleksandr Berezhnyi <oberezhnyi@gmail.com>
2022-10-27 12:52:09 +0300

    Made calculator of aria and perimeter rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    static double side1;
+    static double side2;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть першу сторону прямокутника: ");
+        side1 = scanner.nextDouble();
+
+        System.out.print("Введіть першу сторону прямокутника: ");
+        side2 = scanner.nextDouble();
+
+
+        double rectangle = areaCalculator(side1, side2);
+        double perimeter = perimeterCalculator(side1, side2);
+
+        System.out.println("Площа прямокутника: " + rectangle);
+        System.out.println("Периметр прямокутника: " + perimeter);
+    }
+
+    static double areaCalculator(double side1, double side2){
+        return side1 * side2;
+    }
+
+   static double perimeterCalculator(double side1, double side2) {
+        return 2 * (side1 + side2);
+    }
+}
